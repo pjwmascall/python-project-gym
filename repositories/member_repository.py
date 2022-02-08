aa22c8a533d7430f9bd6048025378f9e80a1a42e
Paul Mascall <pjwmascall@hotmail.com>
2022-02-08 11:07:14 +0000

    added generic SQL queries for member_repository

---
+++ repositories/member_repository.py
@@ -0,0 +1,65 @@
+from db.run_sql import run_sql
+from models.member import Member
+
+
+def save(member):
+    sql = """
+        INSERT INTO
+        members(
+            first_name,
+            last_name,
+            home_address,
+            membership_type,
+            is_active
+            )
+        VALUES (%s, %s, %s, %s, %s)
+        RETURNING id
+        """
+    values = [
+        member.first_name,
+        member.last_name, 
+        member.home_address,
+        member.membership_type,
+        member.is_active
+        ]
+    results = run_sql(sql, values)
+    member.id = results[0]['id']
+    return member
+
+
+def select_all():
+    members = []
+    sql = "SELECT * FROM members"
+    results = run_sql(sql)
+    for row in results:
+        member = Member(
+        row['first_name'],
+        row['last_name'],
+        row['home_address'],
+        row['membership_type'],
+        row['is_active'],
+        row['id']
+        )
+        members.append(member)
+    return members
+
+
+def select(id):
+    member = None
+    sql = "SELECT * FROM members WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+    if result is not None:
+        member = Member(
+        result['first_name'],
+        result['last_name'],
+        result['home_address'],
+        result['membership_type'],
+        result['is_active'],
+        result['id'])
+    return member
+
+
+def delete_all():
+    sql = "DELETE FROM members"
+    run_sql(sql)
