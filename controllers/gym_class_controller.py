1810c8e06eb5a8ebfeed155bbe6fd5c7c02fb24e
Paul Mascall <pjwmascall@hotmail.com>
2022-02-11 19:36:10 +0000

    added new and edit routes to members and classes

--- controllers/gym_class_controller.py
+++ controllers/gym_class_controller.py
@@ -3,14 +3,23 @@ from flask import Blueprint
 from models.gym_class import GymClass
 import repositories.gym_class_repository as gym_class_repository
 
-gym_classes_blueprint = Blueprint("gym_classes", __name__)
+gym_classes_blueprint = Blueprint('gym_classes', __name__)
 
-@gym_classes_blueprint.route("/classes")
-def gym_classes():
+@gym_classes_blueprint.route('/classes', methods = ['GET'])
+def show_gym_classes():
     gym_classes = gym_class_repository.select_all()
-    return render_template("classes/index.html", gym_classes = gym_classes)
+    return render_template('classes/index.html', gym_classes = gym_classes)
 
-@gym_classes_blueprint.route("/classes/<id>")
-def show(id):
+@gym_classes_blueprint.route('/classes/<id>', methods = ['GET'])
+def show_gym_class(id):
     gym_class = gym_class_repository.select(id)
-    return render_template("classes/show.html", gym_class = gym_class)
+    return render_template('classes/show.html', gym_class = gym_class)
+
+@gym_classes_blueprint.route('/classes/new', methods = ['GET'])
+def new_gym_class():
+    return render_template('member/new.html')
+
+@gym_classes_blueprint.route('/classes/<id>/edit', methods = ['GET'])
+def edit_gym_class(id):
+    gym_class = gym_class_repository.select(id)
+    return render_template('classes/edit.html', gym_class = gym_class)
--- controllers/member_controller.py
+++ controllers/member_controller.py
@@ -3,14 +3,23 @@ from flask import Blueprint
 from models.member import Member
 import repositories.member_repository as member_repository
 
-members_blueprint = Blueprint("members", __name__)
+members_blueprint = Blueprint('members', __name__)
 
-@members_blueprint.route("/members")
-def members():
+@members_blueprint.route('/members', methods = ['GET'])
+def show_members():
     members = member_repository.select_all()
-    return render_template("members/index.html", members = members)
+    return render_template('members/index.html', members = members)
 
-@members_blueprint.route("/members/<id>")
-def show(id):
+@members_blueprint.route('/members/<id>', methods = ['GET'])
+def show_member(id):
     member = member_repository.select(id)
-    return render_template("members/show.html", member = member)
+    return render_template('members/show.html', member = member)
+
+@members_blueprint.route('/members/new', methods = ['GET'])
+def new_member():
+    return render_template('member/new.html')
+
+@members_blueprint.route('/members/<id>/edit', methods = ['GET'])
+def edit_member(id):
+    member = member_repository.select(id)
+    return render_template('member/edit.html', member = member)
