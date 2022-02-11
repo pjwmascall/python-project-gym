449da546246b218551f8a92aed8aa157e1b5606b
Paul Mascall <pjwmascall@hotmail.com>
2022-02-11 20:45:59 +0000

    added form and route to create new member

--- controllers/member_controller.py
+++ controllers/member_controller.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
+
 from models.member import Member
+
 import repositories.member_repository as member_repository
 
 members_blueprint = Blueprint('members', __name__)
@@ -18,6 +20,17 @@ def show_member(id):
 @members_blueprint.route('/members/new', methods = ['GET'])
 def new_member():
     return render_template('members/new.html')
+
+@members_blueprint.route('/members', methods=['POST'])
+def create_member():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    home_address = request.form['home_address']
+    membership_type = request.form['membership_type']
+    is_active = request.form['is_active']
+    member = Member(first_name, last_name, home_address, membership_type, is_active)
+    member_repository.save(member)
+    return redirect('/members')
 
 @members_blueprint.route('/members/<id>/edit', methods = ['GET'])
 def edit_member(id):
